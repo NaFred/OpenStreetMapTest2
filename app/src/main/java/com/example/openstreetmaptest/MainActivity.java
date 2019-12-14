59ca2ab648ed581ec05fa42d6246189ca73cf975
Frederik <NaFred.eit@web.de>
2019-12-14 19:07:54 +0100

    Sechseck um den standort, bei wechelndem standort mehrere marker

--- app/src/main/java/com/example/openstreetmaptest/MainActivity.java
+++ app/src/main/java/com/example/openstreetmaptest/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.openstreetmaptest;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+
+
 
 import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.api.IMapController;
@@ -19,23 +33,48 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.OverlayItem;
+
+
+import org.osmdroid.views.overlay.OverlayManager;
+import org.osmdroid.views.overlay.Polygon;
+import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends /*Activity*/AppCompatActivity{
+
+
+public class MainActivity extends /*Activity*/AppCompatActivity {
     MapView map = null;
     //MapEventsReceiver receiver;
     //Context context;
 
+    //your items
+    List<GeoPoint> geoPoints = new ArrayList<>();
+    //add your points here
+    //GeoPoint one = new GeoPoint(49.7,6.0);
+    //GeoPoint two = new GeoPoint(49.867141,8.7);
+    //GeoPoint three = new GeoPoint(49.8,8.78);
+
+
+
+
     MapController mapController;
     GeoPoint hochschule = new GeoPoint(49.867141, 8.638066);
     private MyLocationNewOverlay mLocationOverlay;
+    private LocationManager locManager;
+    private LocationListener locListener;
 
 
-    @Override public void onCreate(Bundle savedInstanceState){
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //handle permissions first, before map is created. not depicted here
@@ -49,7 +88,12 @@ public class MainActivity extends /*Activity*/AppCompatActivity{
         //see also StorageUtils
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+
+//map listener
+        //map events overlay
+        //on single tab auf map events overlay und events weiterreichen
 
 
         //inflate and create the map
@@ -71,13 +115,110 @@ public class MainActivity extends /*Activity*/AppCompatActivity{
 
         mapController.setCenter(hochschule);
         addMarker(hochschule);
-        //map listener
-        //map events overlay
-        //on single tab auf map events overlay und events weiterreichen
+
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                GeoPoint geoPoint = new GeoPoint(location.getLatitude(),location.getLongitude());
+                addMarker(geoPoint);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+
+        Polygon polygon = makeCircle(hochschule,1,map);
+
+        map.getOverlays().clear();
+        getGPS();
+
+        map.getOverlays().add(polygon);
+        addMarker(hochschule);
+        map.invalidate();
+
+
 
 
         //map.setOnTouchListener(this);
+
+    };
+    //Check permission results
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                getGPS();
+                break;
+            default:
+                break;
+        }
     }
+
+    private void getGPS(){
+        //check permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        locManager.requestLocationUpdates("gps", 5000, 0, locListener);
+    }
+
+
+
+    private Polygon makeCircle(GeoPoint geoPoint, double radius, MapView map){
+        //reset geopints for circle
+        geoPoints.clear();
+
+        double radius2 =radius *0.002;
+        double xplus= (geoPoint.getLatitude()+radius2);
+        double xminus= (geoPoint.getLatitude()-radius2);
+        double yplus = (geoPoint.getLongitude()+radius2);
+        double yplusdouble = (geoPoint.getLongitude()+(2*radius2));
+        double yminus = (geoPoint.getLongitude()-radius2);
+        double yminusdouble = (geoPoint.getLongitude()-(2*radius2));
+        double test = geoPoint.getLatitude();
+
+
+        GeoPoint one = new GeoPoint(xplus,yminus);
+        GeoPoint two = new GeoPoint(xplus,yplus);
+        GeoPoint three = new GeoPoint(test,yplusdouble);
+        GeoPoint four = new GeoPoint(xminus,yplus);
+        GeoPoint five = new GeoPoint(xminus,yminus);
+        GeoPoint six = new GeoPoint(test,yminusdouble);
+
+        geoPoints.add(one);
+        geoPoints.add(two);
+        geoPoints.add(three);
+        geoPoints.add(four);
+        geoPoints.add(five);
+        geoPoints.add(six);
+        Polygon polygon = new Polygon(map);
+        polygon.setFillColor(Color.argb(75, 255,0,0));
+        geoPoints.add(geoPoints.get(0));    //forces the loop to close
+        polygon.setPoints(geoPoints);
+        //polygon.setTitle("A sample polygon");
+
+        return polygon;
+    }
+
 
     public void onResume(){
         super.onResume();
@@ -103,7 +244,7 @@ public class MainActivity extends /*Activity*/AppCompatActivity{
         marker.setPosition(center);
         marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
         marker.setTitle("Give it a title");
-        map.getOverlays().clear();
+        //map.getOverlays().clear();
         map.getOverlays().add(marker);
         map.invalidate();
     }
